33bcaf17b44858a8d66b7aa1969abc69ed4bde47
Adrian Palomares <adrianpal45@gmail.com>
2020-09-18 19:18:33 -0700

    Added cors to server

--- server/server.js
+++ server/server.js
@@ -5,23 +5,26 @@ const mongoose = require("mongoose");
 
 const config = require("./_config.js");
 
+const cors = require("cors");
+
 // Connecting mongoose
 mongoose.connect(config.mongoURI[process.env.NODE_ENV], {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    useFindAndModify: false,
 });
 
 app.use(express.json());
+app.use(cors());
 
 // Including routes
 const userRoutes = require("./routes/api/users");
 const carsRoutes = require("./routes/api/cars");
-const authRoutes = require('./routes/api/auth');
+const authRoutes = require("./routes/api/auth");
 
 app.use("/api/users", userRoutes);
 app.use("/api/cars", carsRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 8080;
 
