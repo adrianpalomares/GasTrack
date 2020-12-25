53bdc3ec95b8943a60cfc0edaa71985df4af48f8
Adrian Palomares <adrianpal45@gmail.com>
2020-12-25 14:38:20 -0800

    switched to functional component

--- client/components/pages/Dashboard.js
+++ client/components/pages/Dashboard.js
@@ -1,62 +1,59 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import AuthContext from "../../context/auth";
 
-class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            fuelRecords: [],
-            maintenanceRecords: [],
-        };
-    }
+const Dashboard = () => {
+    // Fuel records and maintenance records
+    const [gasRecords, setGasRecords] = useState([]);
+    const [maintenanceRecords, setMaintenanceRecords] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    componentDidMount() {
-        console.log("Mounted");
-    }
+    useEffect(() => {
+        fetch("http://localhost:8080/api/fuelrecords")
+            .then((res) => res.json())
+            .then((res) => {
+                setGasRecords([res]);
+                setIsLoading(false);
+                console.log(gasRecords);
+            })
+            .catch((err) => console.log(err));
+    }, [isLoading]);
 
-    render() {
-        return (
-            <div class="container">
-                <div class="card text-center">
-                    <div className="card-header">
-                        Saturday September 19, 2020
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Gas</h5>
-                        {/* <p class="card-text">
-                            With supporting text below as a natural lead-in to
-                            additional content.
-                        </p> */}
-                        <p style={{ color: "green", fontSize: "20px" }}>
-                            30.2 mpg
-                        </p>
-                        <a href="#" class="btn btn-primary">
-                            View Details
-                        </a>
-                    </div>
-                    <div class="card-footer text-muted">1990 Mazda MX-5</div>
+    return (
+        <div className="container">
+            <div className="card text-center">
+                <div className="card-header">Saturday September 19, 2020</div>
+                <div className="card-body">
+                    <h5 className="card-title">Gas</h5>
+                    {/* <p class="card-text">
+                        With supporting text below as a natural lead-in to
+                        additional content.
+                    </p> */}
+                    <p style={{ color: "green", fontSize: "20px" }}>30.2 mpg</p>
+                    <a href="#" className="btn btn-primary">
+                        View Details
+                    </a>
                 </div>
-                <br></br>
-                <div class="card text-center">
-                    <div class="card-header">Saturday September 19, 2020</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Maintenance</h5>
-                        {/* <p class="card-text">
-                            With supporting text below as a natural lead-in to
-                            additional content.
-                        </p> */}
-                        <p style={{ color: "green", fontSize: "20px" }}>
-                            30.2 mpg
-                        </p>
-                        <a href="#" class="btn btn-primary">
-                            View Details
-                        </a>
-                    </div>
-                    <div class="card-footer text-muted">1990 Mazda MX-5</div>
-                </div>
+                <div className="card-footer text-muted">1990 Mazda MX-5</div>
             </div>
-        );
-    }
-}
+            <br></br>
+            <div className="card text-center">
+                <div className="card-header">Saturday September 19, 2020</div>
+                <div className="card-body">
+                    <h5 className="card-title">Maintenance</h5>
+                    {/* <p class="card-text">
+                        With supporting text below as a natural lead-in to
+                        additional content.
+                    </p> */}
+                    <p style={{ color: "green", fontSize: "20px" }}>30.2 mpg</p>
+                    <a href="#" className="btn btn-primary">
+                        View Details
+                    </a>
+                </div>
+                <div className="card-footer text-muted">1990 Mazda MX-5</div>
+            </div>
+        </div>
+    );
+};
 
 export default Dashboard;
