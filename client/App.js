b0defe989e1c1f295727d3b986a526cb35b2347a
Adrian Palomares <adrianpal45@gmail.com>
2020-12-25 19:28:22 -0800

    set up context

--- client/App.js
+++ client/App.js
@@ -1,5 +1,5 @@
 import { useRoutes } from "hookrouter";
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import Dashboard from "./components/pages/Dashboard";
@@ -8,21 +8,22 @@ import NotFoundPage from "./components/pages/NotFoundPage";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 // TODO: Implemnt context to store accessToken
-// const routes = {
-//     "/login": () => <Login />,
-//     "/register": () => <Register />,
-//     "/dashboard": () => <Dashboard />,
-// };
+export const AuthContext = createContext();
 
 const App = () => {
+    // State
+    const [accessToken, setAccessToken] = useState();
+
     return (
         <Router>
-            <Switch>
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
-                <Route component={NotFoundPage} />
-            </Switch>
+            <AuthContext.Provider value={{ accessToken, setAccessToken }}>
+                <Switch>
+                    <Route exact path="/dashboard" component={Dashboard} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </AuthContext.Provider>
         </Router>
     );
 };
