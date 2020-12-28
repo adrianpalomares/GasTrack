860b9fe610b192302b06153351c4556d5494fce1
Adrian Palomares <adrianpal45@gmail.com>
2020-12-28 13:10:32 -0800

    updated useLocalStorage

--- client/helper/useLocalStorage.js
+++ client/helper/useLocalStorage.js
@@ -1,30 +1,11 @@
 import { useEffect, useState } from "react";
 
-export default (key, value) => {
-    const [state, setState] = useState(null);
+export default (key) => {
+    const [state, setState] = useState(localStorage.getItem(key));
 
-    useEffect(() => {
-        // Value exists
-        if (localStorage.getItem(key) != null) {
-            setState(localStorage.getItem(key));
-        } else {
-            localStorage.setItem(key, value);
-            setState(value);
-        }
-    }, []);
-
-    const setStorage = (value) => {
-        if (value == null) {
-            removeStorage();
-        } else {
-            setState(value);
-            const stringifiedValue = JSON.stringify(value);
-            localStorage.setItem(key, stringifiedValue);
-        }
-    };
-
-    const removeStorage = () => {
-        localStorage.removeItem(key);
+    const setStorage = (item) => {
+        localStorage.setItem(key, item);
+        setState(item);
     };
 
     return [state, setStorage];
