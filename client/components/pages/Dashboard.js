9bea3ed5f080510840e212af1caed7d9f92b617c
Adrian Palomares <adrianpal45@gmail.com>
2020-12-29 22:24:06 -0800

    Generates component from request data

--- client/components/pages/Dashboard.js
+++ client/components/pages/Dashboard.js
@@ -15,28 +15,25 @@ const Dashboard = () => {
         }).then((res) => {
             setFuelRecords(res.data);
             setIsLoading(false);
+            console.log(fuelRecords);
         });
     }, [isLoading]);
 
     return (
         <div className="container">
-            <FuelRecord />
-            <br></br>
-            <div className="card text-center">
-                <div className="card-header">Saturday September 19, 2020</div>
-                <div className="card-body">
-                    <h5 className="card-title">Maintenance</h5>
-                    {/* <p class="card-text">
-                        With supporting text below as a natural lead-in to
-                        additional content.
-                    </p> */}
-                    <p style={{ color: "green", fontSize: "20px" }}>30.2 mpg</p>
-                    <a href="#" className="btn btn-primary">
-                        View Details
-                    </a>
-                </div>
-                <div className="card-footer text-muted">1990 Mazda MX-5</div>
-            </div>
+            {fuelRecords.map((fuelRecord) => (
+                <FuelRecord
+                    key={fuelRecord._id}
+                    car={fuelRecord.car}
+                    createdAt={fuelRecord.createdAt}
+                    date={fuelRecord.date}
+                    fullTank={fuelRecord.fullTank}
+                    gallons={fuelRecord.gallons}
+                    odometer={fuelRecord.odometer}
+                    pricePerGallon={fuelRecord.pricePerGallon}
+                    updatedAt={fuelRecord.updatedAt}
+                />
+            ))}
         </div>
     );
 };
