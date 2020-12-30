fad73485a656ae9bbc5b75c409cd55499ead1c0b
Adrian Palomares <adrianpal45@gmail.com>
2020-12-29 17:58:52 -0800

    naming and switched to axios

--- client/components/pages/Dashboard.js
+++ client/components/pages/Dashboard.js
@@ -1,40 +1,26 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
+import FuelRecord from "./FuelRecord";
 
 const Dashboard = () => {
     // Fuel records and maintenance records
-    const [gasRecords, setGasRecords] = useState([]);
-    const [maintenanceRecords, setMaintenanceRecords] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const [fuelRecords, setFuelRecords] = React.useState([]);
+    const [maintenanceRecords, setMaintenanceRecords] = React.useState([]);
+    const [isLoading, setIsLoading] = React.useState(true);
 
-    useEffect(() => {
-        fetch("http://localhost:8080/api/fuelrecords")
-            .then((res) => res.json())
-            .then((res) => {
-                setGasRecords(res);
-                setIsLoading(false);
-                console.log(gasRecords);
-            })
-            .catch((err) => console.log(err));
+    React.useEffect(() => {
+        axios({
+            url: "http://localhost:8080/api/fuelrecords",
+            method: "GET",
+        }).then((res) => {
+            setFuelRecords(res.data);
+            setIsLoading(false);
+        });
     }, [isLoading]);
 
     return (
         <div className="container">
-            <div className="card text-center">
-                <div className="card-header">Saturday September 19, 2020</div>
-                <div className="card-body">
-                    <h5 className="card-title">Gas</h5>
-                    {/* <p class="card-text">
-                        With supporting text below as a natural lead-in to
-                        additional content.
-                    </p> */}
-                    <p style={{ color: "green", fontSize: "20px" }}>30.2 mpg</p>
-                    <a href="#" className="btn btn-primary">
-                        View Details
-                    </a>
-                </div>
-                <div className="card-footer text-muted">1990 Mazda MX-5</div>
-            </div>
+            <FuelRecord />
             <br></br>
             <div className="card text-center">
                 <div className="card-header">Saturday September 19, 2020</div>
